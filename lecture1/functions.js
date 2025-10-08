293e3f6fc52e25b36f5e03e6d6b703c4a76e79aa
mahakg09 <gargmahak813@gmail.com>
2025-10-08 18:34:26 +0530

    add all files

---
+++ lecture1/functions.js
@@ -0,0 +1,22 @@
+//function expression
+let sum=function(a,b)
+{
+    return a+b;
+}
+console.log(sum(2,3));
+
+//normal way
+
+function sumfind(a,b,c)
+{
+    return a+b+c;
+}
+console.log(sumfind(3,1,2));
+
+//using arrows
+
+const dosub=(a,b)=>
+{
+    return a-b;
+}
+console.log(dosub(5,6));
